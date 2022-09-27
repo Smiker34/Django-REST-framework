ba2ccaaab6af178582fc26e0c6ad0eb754895b05
Smiker34 <Smiker1@yandex.ru>
2022-09-27 20:08:57 +0300

    Tasks

--- todo/TODO/views.py
+++ todo/TODO/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Project, TODO
 from .serializers import ProjectModelSerializer, TODOModelSerializer
 
 
+class ProjectPagination(PageNumberPagination):
+    page_size = 10
+
+
 class ProjectModelViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectPagination
+
+    def get_queryset(self):
+        queryset = Project.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name:
+            queryset = queryset.filter(name__contains=name)
+        return queryset
+
+
+class ToDoPagination(PageNumberPagination):
+    page_size = 20
 
 
 class TODOModelViewSet(ModelViewSet):
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+    pagination_class = ToDoPagination
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance.is_active = False
+            instance.save()
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
