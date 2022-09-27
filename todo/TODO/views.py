303054ccc0d76aa1fb0c9af68d7c6aa28bb8a77e
Smiker34 <Smiker1@yandex.ru>
2022-09-27 19:26:16 +0300

    Tasks

--- todo/TODO/views.py
+++ todo/TODO/views.py
@@ -1,15 +1,45 @@
+from typing import ItemsView
+from urllib import request
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from .models import Project, TODO
 from .serializers import ProjectModelSerializer, TODOModelSerializer
 
 
+class ProjectPagination(PageNumberPagination):
+    page_size = 10
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectPagination
+
+    def get_queryset(self):
+        queryset = Project.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name:
+            queryset = queryset.filter(name__contains=name)
+        return queryset
+
+
+class TODOPagination(PageNumberPagination):
+    page_size = 20
 
 
 class TODOModelViewSet(ModelViewSet):
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+    pagination_class = TODOPagination
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance.is_active = False
+            instance.save()
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
--- todo/users/views.py
+++ todo/users/views.py
@@ -1,11 +1,15 @@
-from re import T
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ModelViewSet
 
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(mixins.ListModelMixin,
+                       mixins.RetrieveModelMixin,
+                       mixins.UpdateModelMixin,
+                       viewsets.GenericViewSet
+                       ):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
